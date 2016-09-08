3a0215faa6b6c12517a117c6ee577d44e6896aa2
Mikeprod <mikeprod.tm@gmail.com>
2016-09-08 11:55:15 +0200

    Resolving all conflicts with current Mille repo

--- locales/enUS.lua
+++ locales/enUS.lua
@@ -2,9 +2,6 @@ local AddOnName, Engine = ...;
 local AceLocale = LibStub:GetLibrary("AceLocale-3.0");
 local L = AceLocale:NewLocale(AddOnName, "enUS", true, false);
 
-L['General'] = true;
-L['Text'] = true;
-
 L['Modules'] = true;
 L['Left-Click'] = true;
 L['Right-Click'] = true;
@@ -17,11 +14,9 @@ L['Bar Color'] = true;
 L['Use Class Colors for Bar'] = true;
 L['Bar Padding'] = true;
 L['Module Spacing'] = true;
-L['Hide order hall bar'] = true;
 
 -- Positioning Options
 L['Positioning Options'] = true;
-L['Full Screen'] = true;
 L['Horizontal Position'] = true;
 L['Bar Width'] = true;
 L['Left'] = true;
@@ -30,7 +25,6 @@ L['Right'] = true;
 
 -- Media
 L['Font'] = true;
-L['Font Size'] = true;
 L['Small Font Size'] = true;
 L['Text Style'] = true;
 
@@ -38,8 +32,6 @@ L['Text Style'] = true;
 L['Text Colors'] = true;
 L['Normal'] = true;
 L['Inactive'] = true;
-L["Use Class Color for Text"] = true;
-L["Only the alpha can be set with the color picker"] = true;
 L['Use Class Colors for Hover'] = true;
 L['Hover'] = true;
 
@@ -49,22 +41,14 @@ L['Micromenu'] = true;
 L['Show Social Tooltips'] = true;
 L['Main Menu Icon Right Spacing'] = true;
 L['Icon Spacing'] = true;
-L['Guild'] = true;
-L['Online'] = true;
-L['AFK'] = true;
-L['DND'] = true;
 L['Open Guild Page'] = true;
-L['Social'] = true;
 L['No Tag'] = true;
-L['Level'] = true;
 L['Hide Social Text'] = true;
 
-L['Armor'] = true;
 L['Always Show Item Level'] = true;
 L['Minimum Durability to Become Active'] = true;
 L['Maximum Durability to Show Item Level'] = true;
 
-L['Clock'] = true;
 L['Time Format'] = true;
 L['Use Server Time'] = true;
 L['New Event!'] = true;
@@ -80,28 +64,21 @@ L['Ready'] = true;
 L['Travel Cooldowns'] = true;
 L['Change Port Option'] = true;
 
-L['Gold'] = true;
 L['Always Show Silver and Copper'] = true;
-L['Show Free Bag Space'] = true;
 L['Shorten Gold'] = true;
 L['Toggle Bags'] = true;
-L['Total'] = true;
 L['Session Total'] = true;
 
-L['Currency'] = true;
 L['Show XP Bar Below Max Level'] = true;
 L['Use Class Colors for XP Bar'] = true;
 L['Show Tooltips'] = true;
 L['Text on Right'] = true;
-L['Toggle Currency Frame'] = true;
 L['Currency Select'] = true;
 L['First Currency'] = true;
 L['Second Currency'] = true;
 L['Third Currency'] = true;
-L['XP'] = true;
 L['Rested'] = true;
 
-L['System'] = true;
 L['Show World Ping'] = true;
 L['Number of Addons To Show'] = true;
 L['Addons to Show in Tooltip'] = true;
